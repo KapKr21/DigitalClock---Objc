2442eb8fcbba191a3ca7fd7b51057a67df82ffda
Kapil Khatri <kapil@Kapils-Mac-mini.local>
2019-04-10 15:26:19 +0530

    Initial Commit

---
+++ DigitalClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DigitalClock
+//
+//  Created by Kapil Khatri on 10/04/19.
+//  Copyright © 2019 Kapil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
